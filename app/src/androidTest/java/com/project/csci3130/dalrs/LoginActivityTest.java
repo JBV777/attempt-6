8db86b97863f0d9c1d15fe48678159477dc33918
JBV777 <34755273+JBV777@users.noreply.github.com>
2018-06-20 14:15:44 -0300

    Add files via upload

--- app/src/androidTest/java/com/project/csci3130/dalrs/LoginActivityTest.java
+++ app/src/androidTest/java/com/project/csci3130/dalrs/LoginActivityTest.java
@@ -41,7 +41,7 @@ public class LoginActivityTest {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(60000);
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -59,7 +59,7 @@ public class LoginActivityTest {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(60000);
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
